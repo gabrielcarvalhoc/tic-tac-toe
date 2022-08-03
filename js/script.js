a36cdd494e8ed0c81806a7fc9d382763d26dc979
Gabriel <gabrielcarvalho@poli.ufrj.br>
2022-08-03 20:37:28 -0300

    Add function to render contents of gameboard

--- js/script.js
+++ js/script.js
@@ -1,11 +1,27 @@
 const gameBoard = (() => {
-    let board = [];
+    let board = ['X', 'X', 'O', 'X', 'O', 'O', 'O', 'O', 'X'];
+
+    let gridContainer = document.querySelector('.grid-container');
+
+    let gridItems = document.querySelectorAll('.grid-item');
+
+    const renderBoard = () => {
+        board.forEach((element, index) => {
+            gridItems[index].textContent = element;
+        });
+    }
+
+    return {
+        renderBoard
+    };
 })();
+
+gameBoard.renderBoard();
 
 const Player = () => {
     
 };
 
 const gameFlow = (() => {
-    
+
 })();
