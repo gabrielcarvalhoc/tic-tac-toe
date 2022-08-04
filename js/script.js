be147b9bdddcc75265d39318664c6c37ac672f31
Gabriel <gabrielcarvalho@poli.ufrj.br>
2022-08-04 16:19:43 -0300

    Add functions that allows to add marks to spots

--- js/script.js
+++ js/script.js
@@ -1,7 +1,5 @@
 const gameBoard = (() => {
-    let board = ['X', 'X', 'O', 'X', 'O', 'O', 'O', 'O', 'X'];
-
-    let gridContainer = document.querySelector('.grid-container');
+    let board = ['', '', '', '', '', '', '', '', ''];
 
     let gridItems = document.querySelectorAll('.grid-item');
 
@@ -12,16 +10,65 @@ const gameBoard = (() => {
     }
 
     return {
+        board,
+        gridItems,
         renderBoard
     };
 })();
 
-gameBoard.renderBoard();
-
-const Player = () => {
-    
+const Player = (number, mark) => {
+    return {
+        number,
+        mark
+    };
 };
 
-const gameFlow = (() => {
+const player1 = Player('1', 'X');
 
+const player2 = Player('2', 'O');
+
+const gameFlow = (() => {
+    let round = 1;
+    let player1Turn;
+    let player2Turn;
+
+    const playerTurn = () => {
+        if (gameFlow.round % 2 !== 0) {
+            gameFlow.player1Turn = true;
+            gameFlow.player2Turn = false;
+        }
+        if (gameFlow.round % 2 === 0){
+            gameFlow.player1Turn = false;
+            gameFlow.player2Turn = true;
+        }
+    };
+
+    const addMark = () => {
+        gameBoard.gridItems.forEach((item, index) => {
+            item.addEventListener('click', () => {
+                if (gameBoard.board[index] === '') {
+                    playerTurn();
+                    if (gameFlow.player1Turn) {
+                        gameBoard.board.splice(index, 1, player1.mark);
+                        gameBoard.renderBoard();
+                    }
+                    if (gameFlow.player2Turn) {
+                        gameBoard.board.splice(index, 1, player2.mark);
+                        gameBoard.renderBoard();
+                    }
+                    gameFlow.round++;
+                }
+            })
+        })
+    };
+
+    return {
+        playerTurn,
+        addMark,
+        round,
+        player1Turn,
+        player2Turn
+    };
 })();
+
+gameFlow.addMark();
